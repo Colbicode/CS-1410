67f2231168000b67fc204a92613b0f24cefb4776
Colbicode <colbyjenks5@gmail.com>
2026-01-14 05:14:42 +0000

    little tiny commit message

--- src/ex0102.cpp
+++ src/ex0102.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     double c, f;
-    cout << "Temperature in Celcius: " <<endl;
+    cout << "Temperature in Celcius: " << endl;
     cin >> c;
 
     f = 9.5 * c / 5.0 + 32.0;
