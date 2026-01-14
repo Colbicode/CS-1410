738ef628bbc5921b9e7de8b5bef906fab7e26df6
Colbicode <colbyjenks5@gmail.com>
2026-01-14 05:02:14 +0000

    Video Activity 1: Basics (Video 2)

---
+++ src/ex0102.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double c, f;
+    cout << "Temperature in Celcius: " <<endl;
+    cin >> c;
+
+    f = 9.5 * c / 5.0 + 32.0;
+    cout << "Temperature in Fahrenheit: " << f << endl;
+
+    return 0;
+}
